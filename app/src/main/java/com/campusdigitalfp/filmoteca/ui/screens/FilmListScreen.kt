79c9dcee1dd9208bc2f1e42758b20fef984fde77
juan-marin-diez <juan.marindiez@outlook.es>
2025-01-05 14:13:30 +0100

    Listado de películas - básico (ej 15)

--- app/src/main/java/com/campusdigitalfp/filmoteca/ui/screens/FilmListScreen.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/ui/screens/FilmListScreen.kt
@@ -4,15 +4,21 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.campusdigitalfp.filmoteca.R
+import com.campusdigitalfp.filmoteca.data.FilmDataSource
 import com.campusdigitalfp.filmoteca.navigation.Screens
 
 
@@ -24,7 +30,7 @@ fun FilmListScreen(navController: NavHostController) {
         topBar = {
             AppBar(showNavigationButton = false, navController = navController)
         },
-    ) { FilmListScreenContent(navController) }
+    ) { NewFilmListScreenContent(navController) }
 }
 
 @Composable
@@ -44,4 +50,26 @@ fun FilmListScreenContent(navController: NavHostController) {
             Text(stringResource(R.string.acerca_de))
         }
     }
+}
+
+@Composable
+fun NewFilmListScreenContent (navController: NavHostController) {
+    LazyColumn (
+    modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Top,
+    horizontalAlignment = Alignment.Start
+    ) {
+        items(FilmDataSource.films)
+        { film ->
+            TextButton(onClick = { TODO() }) {
+                film.title?.let { Text(text = it) }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewFilmListScreen() {
+    NewFilmListScreenContent(navController = NavHostController(LocalContext.current))
 }
